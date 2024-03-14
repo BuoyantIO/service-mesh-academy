925e911ac6957d942e6cf67821e09c450966d12d
Tom Dean <tom@dean33.com>
2024-03-14 16:41:43 -0500

    Added cluster teardown script

---
+++ eliminate-cross-zone-traffic-hazl/cluster_destroy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# cluster_destroy.sh
+# Demo script for the eliminate-cross-zone-traffic-hazl GitHub repository
+# https://github.com/BuoyantIO/service-mesh-academy/tree/main/eliminate-cross-zone-traffic-hazl
+# Automates cluster deletion and cleans up the hazl and topo kubectl contexts
+# Tom Dean | Buoyant
+# Last edit: 3/14/2024
+
+# Remove the k3d clusters: hazl and topo
+
+k3d cluster delete demo-cluster-orders-hazl
+k3d cluster delete demo-cluster-orders-topo
+k3d cluster list
+
+# Remove the kubectl contexts: hazl and topo
+
+kubectx -d hazl
+kuebctx -d topo
+kubectx
+
+exit 0
