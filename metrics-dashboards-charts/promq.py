9e9ac5189dff3d5cfdb45a20bc24c1a7d252ee8e
Flynn <flynn@buoyant.io>
2024-08-15 10:23:12 -0400

    Make promq do something useful.
    Signed-off-by: Flynn <flynn@buoyant.io>

--- metrics-dashboards-charts/promq.py
+++ metrics-dashboards-charts/promq.py
@@ -1,66 +1,139 @@
 #!python
 
+# SPDX-FileCopyrightText: 2024 Buoyant Inc.
+# SPDX-License-Identifier: Apache-2.0
+
 import sys
 
+import json
 import requests
 import datetime
 import time
 
-def run_promql_query(metric, labels, duration=None, interval=None):
-    query = metric;
+# Class for a Prometheus query. This is pretty much just here to make it
+# simpler to set up the query once, then repeatedly call it.
+class PrometheusQuery:
+    def __init__(self, query: str) -> None:
+        self.query = query
 
-    if labels:
-        query += '{'
-        query += ','.join([f'{k}="{v}"' for k, v in labels.items()])
-        query += '}'
+    def run(self) -> dict:
+        # print(f"---\n{self.query}")
+        response = requests.get('http://localhost:9090/api/v1/query',
+                                params={'query': self.query})
 
-    if duration:
-        query += f'[{duration}'
+        if response.status_code != 200:
+            raise Exception(f'Failed to query Prometheus: {response.text}')
 
-        if interval:
-            query += f':{interval}'
+        data = response.json().get("data", None)
 
-        query += ']'
+        if data is None:
+            raise Exception("No data from Prometheus")
 
-    response = requests.get('http://localhost:9090/api/v1/query', params={'query': query})
+        # print(json.dumps(data, indent=2))
 
-    if response.status_code != 200:
-        raise Exception(f'Failed to query Prometheus: {response.text}')
+        if ((data["resultType"] != "vector") and
+            (data["resultType"] != "matrix")):
+            raise Exception("Response is not a Prometheus vector or matrix")
 
-    data = response.json().get("data", None)
+        return data["result"]
 
-    if data is None:
-        raise Exception("No data from Prometheus")
 
-    if data["resultType"] != "vector":
-        raise Exception("Response is not a Prometheus vector")
+# Run a single query set.
+def get_one_row(queries):
+    values = {}
+    timestamp = None
 
-    return data["result"]
+    for name, query in queries.items():
+        result = query.run()
 
-while True:
-    r1 = run_promql_query("outbound_http_balancer_endpoints",
-                        {
-                            "deployment": "emissary",
-                            "namespace": "emissary",
-                            "backend_name": "face",
-                            "backend_namespace": "faces",
-                        })
+        if len(result) == 0:
+            continue
 
-    for row in r1:
-        metric = row["metric"]
-        deployment = metric["deployment"]
-        namespace = metric["namespace"]
-        state = metric["endpoint_state"]
+        # print(f"{name}: {result}")
 
-        backend = metric["backend_name"]
-        backend_ns = metric["backend_namespace"]
+        # The way we're doing the queries, we'll get an array of
+        # values, but it'll only have one [timestamp, value] entry.
+        rowvalues = result[0]["values"][0]
+        values[name] = float(rowvalues[1])
 
-        timestamp = row["value"][0]
-        value = row["value"][1]
+        # All the timestamps across all our queries should be the
+        # same (that's why we're using a range query rather than an
+        # instantaneous query).
+        rowts = rowvalues[0]
 
-        formatted_timestamp = datetime.datetime.fromtimestamp(timestamp).isoformat()
+        if timestamp is None:
+            timestamp = rowts
+        elif timestamp != rowts:
+            raise Exception("Timestamp mismatch")
 
-        print(f"{formatted_timestamp}: {deployment}.{namespace} -> {backend}.{backend_ns} ({state}): {value}")
+    return (timestamp, values)
 
-    time.sleep(10)
+def main():
+    # Building a query for total gRPC requests from deployment `web` to
+    # parent `voting-svc` in the `emojivoto` namespace, with a 1-minute
+    # duration.
 
+    total_requests = '''
+        sum by (parent_name, parent_namespace) (
+            rate(
+                outbound_grpc_route_backend_response_statuses_total{
+                    deployment="web",
+                    namespace="emojivoto",
+                    parent_name="voting-svc",
+                    parent_namespace="emojivoto"
+                }[1m]
+            )
+        )
+    '''
+
+    # Same, but only for successful requests.
+
+    successful_requests = '''
+        sum by (parent_name, parent_namespace) (
+            rate(
+                outbound_grpc_route_backend_response_statuses_total{
+                    deployment="web",
+                    namespace="emojivoto",
+                    parent_name="voting-svc",
+                    parent_namespace="emojivoto",
+                    grpc_status="OK"
+                }[1m]
+            )
+        )
+    '''
+
+    # Finally, divide those two to get the success rate.
+
+    success_rate = f'({successful_requests} / {total_requests})'
+
+    # Set up PrometheusQuery objects for each query. We're deliberately turning
+    # this into range queries so that Prometheus will sync all their timestamps
+    # for us, and we pick the duration and the resolution to be the same so we get
+    # just a single value.
+    #
+    # (I should probably go vet with the Prometheus folks that this is
+    # more-or-less OK. [ ;) ])
+
+    queries = {
+        "total": PrometheusQuery(total_requests + "[10s:10s]"),
+        "success": PrometheusQuery(successful_requests + "[10s:10s]"),
+        "rate": PrometheusQuery(success_rate + "[10s:10s]"),
+    }
+
+    print("Timestamp            Total     OK    OK %")
+    print("---------            -----  -----  ------")
+    while True:
+        timestamp, values = get_one_row(queries)
+
+        then = datetime.datetime.fromtimestamp(timestamp).isoformat()
+
+        req = values["total"]
+        ok = values["success"]
+        rate = values["rate"] * 100.0
+
+        print("%s:  %3.2f   %3.2f  %6.2f" % (then, req, ok, rate))
+
+        time.sleep(10)
+
+if __name__ == '__main__':
+    main()
